42c046c19a9b8e88e93f3c96a74ae7e2ce821c9e
aleksandGG <aleksandrgaranin89@gmail.com>
2020-10-03 11:42:29 -0700

    Project Created + Logo added

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useEffect, Suspense } from 'react';
+import Layout from './hoc/Layout/Layout';
+import BasketList from './containers/BurgerBuilder/BasketList';
+
+import Logout from './containers/Auth/Logout/Logout';
+
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
+
+const Auth = React.lazy(() => {
+  return import('./containers/Auth/Auth')
+})
+
+const Checkout = React.lazy(() => {
+  return import('./containers/Checkout/Checkout')
+})
+
+const Orders = React.lazy(() => {
+  return import('./containers/Orders/Orders')
+})
+
+
+const App = (props) => {
+  const {onTryAutoSignup} = props;
+  useEffect(() => {
+    onTryAutoSignup();
+  }, [onTryAutoSignup]);
+  
+    let routes = (
+      <Switch> 
+        <Route path="/auth" render={(props) => <Auth {...props}/>}/>
+        <Route path="/" exact component={BasketList}/>
+        <Redirect to="/"/>
+      </Switch>
+    );
+
+    if ( props.isAuthenticated ) {
+      routes = (
+        <Switch> 
+          <Route path="/checkout" render={(props) => <Checkout {...props}/>}/>
+          <Route path="/orders" render={(props) => <Orders {...props}/>}/>            
+          <Route path="/logout" component={Logout}/>
+          <Route path="/auth" render={(props) => <Auth {...props}/>}/>
+          <Route path="/" exact component={BasketList}/>
+          <Redirect to="/"/>
+        </Switch>  
+      )};
+
+    return  (
+      <div>
+        <Layout>
+          <Suspense fallback={<p>Loading...</p>}>
+            {routes}  
+          </Suspense>
+        </Layout>
+      </div>
+    );
+  
+  
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
---
+++ src/containers/BurgerBuilder/BasketList.js
@@ -0,0 +1,119 @@
+import React,{ useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import axios from '../../axios-orders';
+import Aux from '../../hoc/Aux/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+import * as actions from '../../store/actions/index';
+
+
+const BurgerBuilder = props => {
+    const [purchasing, setPurchasing] = useState(false);
+    const { onInitIngredients } = props
+    useEffect(() => {
+        onInitIngredients();
+    }, [onInitIngredients]);
+
+    const updatePurchaseState = (ingredients) => {
+     
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        return sum > 0;
+    }
+
+    const purchaseHandler = () => {
+        if (props.isAuthenticated){
+            setPurchasing(true);
+        } else{
+            props.onSetAuthRedirectPath('/checkout')
+            props.history.push('/auth')
+        }
+        
+    }
+
+    const purchaseCanselHandler = () => {
+        setPurchasing(false)
+    }
+
+    const purchaseContinueHandler = () => {      
+        props.onInitPurchase();  
+        props.history.push('/checkout');
+    }
+
+        const disabledInfo = {
+            ...props.ing
+        };
+        for (let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+        let orderSummary = null
+        
+        let burger = props.error ? <p style={{textAlign:"center"}}>Ingredients can't be loaded</p> : <Spinner/>
+
+        if (props.ing){
+            burger = (
+                <Aux>
+                    <Burger ingredients={props.ing}></Burger>
+                    <BuildControls 
+                        ingredientAdded={props.onIngredientAdded}
+                        ingredientRemoved={props.onIngredientRemoved}
+                        disabled={disabledInfo}
+                        purchaseable={updatePurchaseState (props.ing)}
+                        ordered={purchaseHandler}
+                        isAuth={props.isAuthenticated}
+                        price={props.price}
+                    />       
+                </Aux>
+            )
+            orderSummary = <OrderSummary 
+                ingredients={props.ing}
+                purchaseCanceled={purchaseCanselHandler}
+                purchaseContinued={purchaseContinueHandler}
+                price={props.price}
+            />;
+
+        }       
+       
+        return (            
+            <Aux>                
+                <Modal show={purchasing} modalClosed={purchaseCanselHandler}> 
+                    {orderSummary}
+                </Modal>
+                {burger}
+                    
+            </Aux>
+        );
+    
+}
+
+
+const mapStateToProps = state => {
+    return {
+        ing: state.bbr.ingredients,
+        price: state.bbr.totalPrice,
+        error: state.bbr.error,
+        isAuthenticated: state.auth.token !== null
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
